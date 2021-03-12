d0e88b67a7c051c4b0d5471a231e45ee8580d914
prajwal kumar <62468512+prajwal-creator@users.noreply.github.com>
2021-03-13 00:43:35 +0530

    Fully Updated and final version

--- python_word_remainder.py
+++ python_word_remainder.py
@@ -1,13 +1,20 @@
 import time
 from plyer import notification
 import csv
+import random
 
 if __name__=="__main__":
+    data=[]
+    out=open("complete_dictionary.csv",'r')
+    d = csv.reader(out)
+    for row in d:
+        data.append(row)
     while True:
         notification.notify(
             title = "Learn a new word",
-            message = "petrified-mean scared",
+            message = str(data[random.randint(0,len(data)-1)]),
             app_icon ="C:\\Users\\prajw\\OneDrive\\Desktop\\MY work\\Notification remainder\\dictionary.ico",
             timeout=6
         )
-        time.sleep(60*60)
+        time.sleep(10)
+        out.close()
